e516809b45634a954e4f9579d499c60b7efb55cb
Michael McCann <81657333+MichaelMcCann1@users.noreply.github.com>
2021-09-03 09:33:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,151 +1,165 @@
 // DOM Selectors
-const bird = document.querySelector('.bird');
-const ground = document.querySelector('.ground');
-const birdImg = document.querySelector('#birdImg')
+const skySelector = document.querySelector('.sky')
+const birdSelector = document.querySelector('.bird');
+const groundSelector = document.querySelector('.ground');
+const birdImgSelector = document.querySelector('#birdImg')
 const gameContainer = document.querySelector('.game');
 const body = document.querySelector('body');
 const obstacleContainer = document.querySelector('.obstacleContainer');
 const obstacleContainer2 = document.querySelector('.obstacleContainer2')
-const scoreElement = document.querySelector('.score');
-const gameOverScreen = document.querySelector('.gameOver');
-const gameOverScore = document.querySelector('.gameOverScore');
+const scoreElementSelector = document.querySelector('.score');
+const gameOverScreenSelector = document.querySelector('.gameOver');
+const gameOverScoreSelector = document.querySelector('.gameOverScore');
 const hiscoreSelector = document.querySelector('.hiscore')
+const restartMessageSelector = document.querySelector('.restartMessage')
 const wingUpPath = "Images/Yellow/wing-up.png";
-const wingDownPath = "Images/Yellow/wing-down.png"
+const wingDownPath = "Images/Yellow/wing-down.png";
 
-const birdTop = 200; //initial position of bird
 const gravity = .3 ; //constant value of how quickly gravity pulls you down
-const gravitySpeed = 0; //actual speed that changes
-obstacleContainer.style.bottom = 299 + parseFloat(getComputedStyle(ground).height.slice(0,-2)) + 'px'; //set position of obstacle container
+const birdSpeed = 0; //actual speed of bird. Value constantly changes as you fall
+let initialBirdPosition;
+let game;
 
+window.onload = () => {
+  obstacleContainer.style.bottom = parseFloat(getComputedStyle(groundSelector).height.slice(0,-2)) + 'px'
+  initialBirdPosition = parseFloat(getComputedStyle(birdSelector).top)
 
-class Game {
-  constructor(gravity, gravitySpeed, birdTop, scoreElement) {
-    this.gravity = gravity;
-    this.gravitySpeed = gravitySpeed; 
-    this.birdTop = birdTop;
-    this.scoreElement = scoreElement;
-    this.pull = setInterval(() => {game.pullBird()}, 20);
-    this.generateObstacles = setInterval(() => {game.generateObstacle()}, 2000);
-    this.moveObstacles = setInterval(() => {game.moveObstacle()}, 20);
-    this.hiscore = 0;
-  }
-   
-  pullBird() {
-    this.gravitySpeed += this.gravity;
-    this.updatePosition(this.gravitySpeed);
-    if (bird.getBoundingClientRect().bottom > ground.getBoundingClientRect().top) { //check if bird hits the ground
-      game.gameOverScreenPopup();
+  window.addEventListener('click', () => {
+    if (!game) game = new Game(gravity, birdSpeed, initialBirdPosition, scoreElementSelector)
+    game.jump()
+  })
+  window.addEventListener('keyup', (e) => {
+    if (!game && e.key === ' ') game = new Game(gravity, birdSpeed, initialBirdPosition, scoreElementSelector)
+    game.jump()
+  })
+
+  class Game {
+    constructor(gravity, birdSpeed, birdTop, scoreElement) {
+      this.gravity = gravity;
+      this.birdSpeed = birdSpeed; 
+      this.birdTop = birdTop;
+      this.scoreElement = scoreElement;
+      this.hiscore = 0;
+      this.pull = setInterval(() => {game.pullBird()}, 20);
+      this.generateObstacles = setInterval(() => {game.generateObstacle()}, 2000);
+      this.moveObstacles = setInterval(() => {game.moveObstacle()}, 20);
     }
-  }
-
-  updatePosition(speed){
-    this.birdTop += speed;
-    bird.style.top = this.birdTop + 'px';
-  }
-
-  jump(){
-    this.gravitySpeed = -6;
-    if (/wing-up.png/.test(birdImg.src)) {
-      setTimeout(() => birdImg.src = wingUpPath, 100 );
-    }
-    birdImg.src = wingDownPath;
-  }
-
-  generateObstacle(){
-    let obsHeight = Math.random() * (300 - 100) + 100;
-    let obstacle = document.createElement('div');
-    obstacleContainer.appendChild(obstacle)
-    obstacle.classList.add('obstacle');
-    obstacle.style.height = obsHeight + 'px';
-
-    obstacle = document.createElement('div');
-    obstacleContainer2.appendChild(obstacle)
-    obstacle.classList.add('obstacle', 'obstacle2');
-    obsHeight = 375 - obsHeight;
-    obstacle.style.height = obsHeight + 'px';
-  }
-
-  moveObstacle(){
-    let obstacles = document.querySelectorAll('.obstacle');
-    obstacles.forEach(item => {
-      let obsLeft = parseFloat(getComputedStyle(item).left.slice(0,-2));
-      obsLeft -= 3;
-      item.style.left = obsLeft + 'px'
-      
-      game.checkCollision(item);
-      
-      let dist = item.getBoundingClientRect().left - bird.getBoundingClientRect().left;
-      if (dist <= 3 && dist > 0 && !item.classList.contains('obstacle2')){
-        this.updateScore();
-      }
-    })
-  }
-
-  checkCollision(item){
-    if (!item.classList.contains('obstacle2')){
-      if (item.getBoundingClientRect().left < bird.getBoundingClientRect().right && bird.getBoundingClientRect().bottom > item.getBoundingClientRect().top && bird.getBoundingClientRect().left < item.getBoundingClientRect().right) { //check for collision with obstacle
-        game.gameOverScreenPopup();
+    
+    pullBird() {
+      this.birdSpeed += this.gravity
+      this.updatePosition(this.birdSpeed)
+      if (birdSelector.getBoundingClientRect().bottom > groundSelector.getBoundingClientRect().top) { //check if bird hits the ground
+        game.gameOverScreenPopup()
       }
     }
-    if (item.classList.contains('obstacle2')){
-      if (item.getBoundingClientRect().left < bird.getBoundingClientRect().right && bird.getBoundingClientRect().top < item.getBoundingClientRect().bottom && bird.getBoundingClientRect().left < item.getBoundingClientRect().right) {
-        game.gameOverScreenPopup();
+  
+    updatePosition(speed){
+      this.birdTop += speed;
+      birdSelector.style.top = this.birdTop + 'px';
+    }
+  
+    jump(){
+      this.birdSpeed = -6 //pulls bird up
+      if (birdImgSelector.src.includes('wing-up.png')) {
+        setTimeout(() => birdImgSelector.src = wingUpPath, 100 )
+      }
+      birdImgSelector.src = wingDownPath
+    }
+  
+    generateObstacle(){
+      let obsHeight = Math.random() * (parseFloat(getComputedStyle(obstacleContainer).height) - 100) + 100
+      let obstacle = document.createElement('div')
+      obstacleContainer.appendChild(obstacle)
+      obstacle.classList.add('obstacle')
+      obstacle.style.height = obsHeight + 'px'
+  
+      obstacle = document.createElement('div')
+      obstacleContainer2.appendChild(obstacle)
+      obstacle.classList.add('obstacle', 'obstacle2')
+      obsHeight = parseFloat(getComputedStyle(skySelector).height) - obsHeight - 150
+      obstacle.style.height = obsHeight + 'px'
+    }
+  
+    moveObstacle(){
+      let obstaclesSelector = document.querySelectorAll('.obstacle')
+      obstaclesSelector.forEach(obstacle => {
+        let obstacleLeftPosition = parseFloat(getComputedStyle(obstacle).left.slice(0,-2))
+        obstacleLeftPosition -= 3;
+        obstacle.style.left = obstacleLeftPosition + 'px'
+        
+        game.checkCollision(obstacle)
+        
+        let obstacleBirdDistance = obstacle.getBoundingClientRect().left - birdSelector.getBoundingClientRect().left;
+        if (obstacleBirdDistance <= 3 && obstacleBirdDistance > 0 && !obstacle.classList.contains('obstacle2')){
+          this.updateScore();
+        }
+      })
+    }
+  
+    checkCollision(item){
+      if (!item.classList.contains('obstacle2')){
+        if (item.getBoundingClientRect().left < birdSelector.getBoundingClientRect().right && 
+            birdSelector.getBoundingClientRect().bottom > item.getBoundingClientRect().top && 
+            birdSelector.getBoundingClientRect().left < item.getBoundingClientRect().right) {//check for collision with obstacle
+          game.gameOverScreenPopup();
+        }
+      }
+      if (item.classList.contains('obstacle2')){
+        if (item.getBoundingClientRect().left < birdSelector.getBoundingClientRect().right && 
+            birdSelector.getBoundingClientRect().top < item.getBoundingClientRect().bottom && 
+            birdSelector.getBoundingClientRect().left < item.getBoundingClientRect().right) {
+          game.gameOverScreenPopup();
+        }
       }
     }
-  }
-
-  updateScore(){
-    this.scoreElement.textContent = parseInt(this.scoreElement.textContent) + 1;
-  }
-
-  gameOverScreenPopup(){
-    clearInterval(this.pull);
-    clearInterval(this.generateObstacles);
-    clearInterval(this.moveObstacles);
-    gameOverScreen.style.display = 'flex';
-    gameOverScore.textContent += this.scoreElement.textContent.toString();
-    if (this.scoreElement.textContent > this.hiscore) {
-      this.hiscore = parseInt(this.scoreElement.textContent);
-      hiscoreSelector.textContent = `Best Score: ${this.hiscore}`;
+  
+    updateScore(){
+      this.scoreElement.textContent = parseInt(this.scoreElement.textContent) + 1;
     }
-    window.addEventListener('keydown', this.clearGame);
-  }
-
-  clearGame(event){
-    if (event.key === ' ') {
-      gameOverScreen.style.display = 'none';
-      gameOverScore.textContent = 'Score: ';  
-      scoreElement.textContent = 0;
-      while (obstacleContainer.firstChild) {
-        obstacleContainer.removeChild(obstacleContainer.lastChild);
+  
+    gameOverScreenPopup(){
+      clearInterval(this.pull)
+      clearInterval(this.generateObstacles)
+      clearInterval(this.moveObstacles)
+      gameOverScreenSelector.style.display = 'flex'
+      gameOverScoreSelector.textContent += this.scoreElement.textContent.toString()
+      if (this.scoreElement.textContent > this.hiscore) {
+        this.hiscore = parseInt(this.scoreElement.textContent)
+        hiscoreSelector.textContent = `Best Score: ${this.hiscore}`
       }
-      while (obstacleContainer2.firstChild) {
-        obstacleContainer2.removeChild(obstacleContainer2.lastChild);
-      }
-      game.resetTimers();
-      game.removeClearGameListener();
+      window.innerWidth > 500 ? restartMessageSelector.textContent = 'Press space or click to restart' : restartMessageSelector.textContent = 'Tap to restart'
+      window.addEventListener('keyup', this.clearGame)
+      window.addEventListener('click', this.clearGame)
     }
-  }
-
-  resetTimers(){
-    this.gravitySpeed = 0;
-    this.birdTop = 200;
-    this.pull = setInterval(() => {game.pullBird()}, 20);
-    this.generateObstacles = setInterval(() => {game.generateObstacle()}, 2000);
-    this.moveObstacles = setInterval(() => {game.moveObstacle()}, 20);
-  }
-
-  removeClearGameListener(){
-    window.removeEventListener('keydown', this.clearGame)
+  
+    clearGame(event){
+      if (event.key === ' ' || event.type === "click") {
+        gameOverScreenSelector.style.display = 'none';
+        gameOverScoreSelector.textContent = 'Score: ';  
+        scoreElementSelector.textContent = 0;
+        while (obstacleContainer.firstChild) {
+          obstacleContainer.removeChild(obstacleContainer.lastChild);
+        }
+        while (obstacleContainer2.firstChild) {
+          obstacleContainer2.removeChild(obstacleContainer2.lastChild);
+        }
+        game.resetTimers();
+        game.removeClearGameListener();
+      }
+    }
+  
+    resetTimers(){
+      this.birdSpeed = 0;
+      this.birdTop = initialBirdPosition;
+      this.pull = setInterval(() => {game.pullBird()}, 20);
+      this.generateObstacles = setInterval(() => {game.generateObstacle()}, 2000);
+      this.moveObstacles = setInterval(() => {game.moveObstacle()}, 20);
+      game.jump()
+    }
+  
+    removeClearGameListener(){
+      window.removeEventListener('keyup', this.clearGame)
+      window.removeEventListener('click', this.clearGame)
+    }
   }
 }
-
-
-let game = new Game(gravity, gravitySpeed, birdTop, scoreElement); 
-
-window.addEventListener('keyup', event => { 
-    if (event.key === ' ') {
-    game.jump();
-  }
-})
